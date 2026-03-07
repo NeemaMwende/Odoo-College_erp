2802803b60ee8036fe2a48aa924aa6c4b5c88db7
NeemaMwende <gracepete12@gmail.com>
2026-03-07 18:01:37 +0300

    feat: manifest configuration

--- college_erp/__manifest__.py
+++ college_erp/__manifest__.py
@@ -1,7 +1,16 @@
 {
     'name': 'College ERP',
     'version': '18.0.1.1',
+    'license' : 'LGDL-3',
     'author': 'Neema Mwende',
+    'category': 'Education',
     'summary': 'An erp for college education',
-    'description': """From students administration to exam, this covers all aspects of college administratioon"""
+    'description': """From students administration to exam, this covers all aspects of college administratioon""",
+    'website': 'https://github.com/NeemaMwende',
+    'maintainer': 'Mi casa su casa <neemamwende009@gmail.com>',
+    'sequence': 1,
+    'application': True,
+    'installable': True,
+    'auto_install': True,
+
 }
